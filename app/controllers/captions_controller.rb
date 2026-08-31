e35c80b3534b4016750b89908a657eb8b0d3e785
adita111 <152799340+adita111@users.noreply.github.com>
2026-08-31 12:50:34 +0300

    Added REST endpoints for captions

---
+++ app/controllers/captions_controller.rb
@@ -0,0 +1,32 @@
+class CaptionsController < ApplicationController
+  skip_forgery_protection
+  def index
+    caption = Caption.all
+    render json: caption
+  end
+
+  def show
+    caption = Caption.find(params[:id])
+    render json: caption
+  end
+
+  def create
+    caption = Caption.create(caption_params)
+
+    render json: caption, status: :created
+  end
+
+  def destroy
+    caption = Caption.find(params[:id])
+    caption.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def caption_params
+    params.require(:caption).permit(:source_url, :text, :image_path)
+  end
+
+end
